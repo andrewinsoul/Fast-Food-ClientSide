0f8ebde694212027cc2ab44d4a2b53ab0add1a8b
andrewinsoul <andrewinsoul@gmail.com>
2019-02-04 05:44:22 +0100

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 ### Fast Food Client Side
-This project uses React to consume the Fast-Food-API endpoint which is available on: https://fast-food-andy.herokuapp.com/api/v1/api-docs/
+This project uses React to consume the Fast-Food-API backend which is available on: https://fast-food-andy.herokuapp.com/api/v1/api-docs/
+
+#### GETTING STARTED
+Read on belowm to see what you need to run the application and make contributions to it's progress.
+
+#### PREQUISITES
+To successfully run this application on your local machine, you need to ensure the following: 
+ - Node is installed on ypur local machine;
+ - Refer to the package.json file for other dependencies you need to install
+
+#### INSTALLATION
+- To install this application, scroll to the project's repo and click on the big green button to clone or download the repository. Alternatively, you can use the git clone command by running `git clone https://github.com/andrewinsoul/Fast-Food-Clientside.git`
+Once the application is installed, you nust run npm install to install all dependencies and deveDependencies. After installation of all dependencies used in the app, you can view the app locally by running npm start script.
+NB: Running npm start will automatically open your default browser and load the app there. 
+
+#### TECHNOLOGIES USED
+- React: React is a framework that entirely focuses on the UI of the app. It's component archictectural design has made it stand out from other competitors. 
+- Redux: Redux is  a library that curbs complexities that might arise when trying to manage state across components.
+- Heroku: The application was hosted on heroku and it can be accessed from:
+<a href="https://andrew667.herokuapp.com"> this link</a>
+- Pivotal Tracker: The progress of this application is tracked through the use of Pivotal Tracker management tool. The link to the board on Pivotal Tracker tracking the progress of this project can be accessed from <a href="https://www.pivotaltracker.com/n/projects/2226230">here</a>
+
+#### HOW TO CONTRIBUTE
+To contribute to this project
+- Fork the repo
+- Work on the forked repo
+- Ensure nothing is breaking before raising a pull request
+- Raise a pull request
+<h3>Your contributions are welcome</h3>
--- client/tests/Actions/cartActions.test.js
+++ client/tests/Actions/cartActions.test.js
@@ -1,56 +1,78 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import moxios from 'moxios';
-import sinon from 'sinon';
-import * as cartActions from '../../actions/cartAction';
-import * as types from '../../actions/types';
-import * as apiRequests from '../../utilities/apiCalls';
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import moxios from "moxios";
+import sinon from "sinon";
+import * as cartActions from "../../actions/cartAction";
+import * as types from "../../actions/types";
+import * as apiRequests from "../../utilities/apiCalls";
+import axios from 'axios';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 jest.setTimeout(30000);
 
-describe('test for actions related to cart', () => {
+describe("test for actions related to cart", () => {
   beforeEach(() => {
     moxios.install();
   });
   afterEach(() => {
     moxios.uninstall();
   });
-  it('dispatches ADD_TO_CART action', async () => {
-    const expectedAction = [{
-      type: types.ADD_TO_CART,
-      foodItem: 1
-    }];
+  it("dispatches ADD_TO_CART action", async () => {
+    const expectedAction = [
+      {
+        type: types.ADD_TO_CART,
+        foodItem: 1
+      }
+    ];
     const store = mockStore({ cart: {} });
     await store.dispatch(cartActions.AddToCart(1));
     expect(store.getActions()).toEqual(expectedAction);
   });
-  it('dispatches REMOVE_FROM_CART', async () => {
-    const expectedAction = [{
-      type: types.REMOVE_FROM_CART,
-      foodId: 2
-    }];
+  it("dispatches REMOVE_FROM_CART", async () => {
+    const expectedAction = [
+      {
+        type: types.REMOVE_FROM_CART,
+        foodId: 2
+      }
+    ];
     const store = mockStore({ cart: {} });
     await store.dispatch(cartActions.RemoveFromCart(2));
     expect(store.getActions()).toEqual(expectedAction);
   });
-  it('dispatches ADD_QTY action', async () => {
-    const expectedAction = [{
-      type: types.ADD_QTY,
-      foodId: 2
-    }];
+  it("dispatches ADD_QTY action", async () => {
+    const expectedAction = [
+      {
+        type: types.ADD_QTY,
+        foodId: 2
+      }
+    ];
     const store = mockStore({ cart: {} });
     await store.dispatch(cartActions.AddQty(2));
     expect(store.getActions()).toEqual(expectedAction);
   });
-  it('dispatches SUBTRACT_QTY action', async () => {
-    const expectedAction = [{
-      type: types.SUBTRACT_QTY,
-      foodId: 1
-    }];
+  it("dispatches SUBTRACT_QTY action", async () => {
+    const expectedAction = [
+      {
+        type: types.SUBTRACT_QTY,
+        foodId: 1
+      }
+    ];
     const store = mockStore({ cart: {} });
     await store.dispatch(cartActions.SubtractQty(1));
     expect(store.getActions()).toEqual(expectedAction);
+  });
+  it("dispatches PLACE_ORER action", async () => {
+    const dispatch = jest.fn();
+    jest.spyOn(
+      apiRequests.Post, ('/orders', [])
+    ).mockImplementation(() => Promise.resolve({ status: 201 }));
+    const response = { data: { message: "placed" } };
+    // jest
+    //   .spyOn(axios, 'post')
+    //   .mockImplementation(() => Promise.resolve({ status: 201 }));
+    await cartActions.PlaceOrder([1, 2])(dispatch);
+    expect(dispatch.mock.calls[1]).toEqual({ type: "PLACE_ORDER" });
+    expect(dispatch).toHaveBeenCalledTimes(3);
   });
 });
